7f7aee9ee9fcf248969933c0fa02e6cf9a9e7237
RobKHX <rokhero@protonmail.com>
2024-03-14 18:18:47 +0100

    Adjust web view.

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
 
 def add_todo():
     todo = st.session_state['new_todo'] + "\n"
@@ -12,7 +13,13 @@ def add_todo():
 
 st.title("Moje aplikace Úkoly")
 st.subheader("Moje první webová aplikace.")
-st.write("Tato aplikace ti pomůže organizovat své úkoly.")
+st.write("Tato aplikace ti pomůže <b>zvýšit produktivitu</b>.",
+         unsafe_allow_html=True)
+
+
+st.text_input(label="", placeholder="Přidej nový úkol...: ",
+              on_change=add_todo, key='new_todo')
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -21,6 +28,3 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.experimental_rerun()
-
-st.text_input(label="", placeholder="Přidej nový úkol: ",
-              on_change=add_todo, key='new_todo')
